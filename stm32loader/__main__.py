e2d4f840efc626dd5b5a005b27500f505ee6964e
Floris Lambrechts <florisla@gmail.com>
2019-04-17 22:15:54 +0200

    Add header to __main__.py

--- stm32loader/__main__.py
+++ stm32loader/__main__.py
@@ -1,3 +1,21 @@
+# Author: Floris Lambrechts
+# GitHub repository: https://github.com/florisla/stm32loader
+#
+# This file is part of stm32loader.
+#
+# stm32loader is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 3, or (at your option) any later
+# version.
+#
+# stm32loader is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with stm32loader; see the file LICENSE.  If not see
+# <http://www.gnu.org/licenses/>.
 """
 Execute stm32loader as a module.
 
