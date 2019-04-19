9da03ca64adfb6d197e5ef07f79764d48b38933b
Floris Lambrechts <florisla@gmail.com>
2019-04-19 21:55:27 +0200

    Release: bump version number from v0.3.3-dev to v0.4.0-dev

--- stm32loader/__init__.py
+++ stm32loader/__init__.py
@@ -1,4 +1,4 @@
 """Flash firmware to STM32 microcontrollers over a serial connection."""
 
-__version_info__ = (0, 3, 3, "dev")
+__version_info__ = (0, 4, 0, "dev")
 __version__ = "-".join(str(part) for part in __version_info__).replace("-", ".", 2)
