2ae257de17c124fcb6abae96acd49ba503184985
Floris Lambrechts <florisla@gmail.com>
2023-10-09 22:43:06 +0200

    release: Bump version to 0.7.0

--- stm32loader/__init__.py
+++ stm32loader/__init__.py
@@ -1,4 +1,4 @@
 """Flash firmware to STM32 microcontrollers over a serial connection."""
 
-__version_info__ = (0, 6, 0)
+__version_info__ = (0, 7, 0)
 __version__ = "-".join(str(part) for part in __version_info__).replace("-", ".", 2)
